ad4e995169bf6380bcdb874a1f08c3d1768ac343
PavanUngarala <pavankumarungarala@gmail.com>
2025-11-25 21:42:22 +0530

    New project initial commit (entire project structure)

---
+++ src/com/infy/functions/Calculator.java
@@ -0,0 +1,6 @@
+package com.infy.functions;
+
+@FunctionalInterface
+public interface Calculator {
+	public float calculate(int a, int b);
+}
---
+++ src/com/infy/main/CalculatorMain.java
@@ -0,0 +1,43 @@
+package com.infy.main;
+
+import java.util.Scanner;
+
+import com.infy.expression.CalculationExpressions;
+import com.infy.functions.Calculator;
+
+public class CalculatorMain {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		CalculationExpressions calc = new CalculationExpressions();
+		System.out.println("Welcome to calculator functional interface usease");
+		System.out.println("Select your operation: ");
+		System.out.print("1. Addition");
+		System.out.print("2. Subtraction");
+		System.out.print("3. Multiplication");
+		System.out.print("4. Division");
+		System.out.print("Enter your option: ");
+		int opt = scan.nextInt();
+		switch(opt) {
+		case 1:
+			System.out.println("Enter the values");
+			System.out.print("number1: ");
+			int n1 = scan.nextInt();
+			System.out.print("number2: ");
+			int n2 = scan.nextInt();
+			Calculator calculator = (a,b) -> a+b;
+			System.out.println("Addition of these two numbers are: "+ calc.calculate(calculator, n1, n2));
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			System.out.println("Please enter correct option");
+		}
+
+	}
+
+}
